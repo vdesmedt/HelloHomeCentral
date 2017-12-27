d415448a56f6d38da533d58be1e04ef4eb43f9c5
Valery De Smedt <valery@indri.be>
2017-12-27 14:12:57 +0100

    Project Structure

---
+++ HelloHome.Central.Hub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloHome.Central.Hub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
