2d280faa009ca6643d9529fe27a6936a8574cf87
Valery De Smedt <valery@indri.be>
2018-01-03 15:54:47 +0100

    Added SerialPortStream dependency

--- HelloHome.Central.Hub/MessageChannel/SerialPortMessageChannel/SerialPortMessageChannel.cs
+++ HelloHome.Central.Hub/MessageChannel/SerialPortMessageChannel/SerialPortMessageChannel.cs
@@ -1,10 +1,22 @@
 ﻿using System.Threading;
 using HelloHome.Central.Hub.MessageChannel.Messages;
+using Microsoft.Extensions.Configuration;
+using RJCP.IO.Ports;
 
 namespace HelloHome.Central.Hub.MessageChannel.SerialPortMessageChannel
 {
     public class SerialPortMessageChannel : IMessageChannel
     {
+        private SerialPortStream _port { get; set; }
+
+        public SerialPortMessageChannel(IConfigurationRoot config)
+        {
+            _port = new SerialPortStream(config["messageChannel:serial:port"],
+                config.GetValue<int>("messageChannel:seria:baud"), 8, Parity.None, StopBits.One);
+            _port.Open();
+        }
+
+
         public IncomingMessage TryReadNext(int millisecond, CancellationToken cancellationToken)
         {
             throw new System.NotImplementedException();
