896b133d22b02378127408a8887b0a01c8514125
Valery De Smedt <valery@indri.be>
2018-01-04 21:22:27 +0100

    Add Typed Exception

---
+++ HelloHome.Central.Common/Exceptions/HelloHomeException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloHome.Common.Exceptions
+{
+    [Serializable]
+    public class HelloHomeException : Exception
+    {
+        public string Code { get; private set; }
+
+        public HelloHomeException(string code)
+            : base()
+        {
+            Code = code;
+        }
+        public HelloHomeException(string code, string message)
+            : base(message)
+        {
+            Code = code;
+        }
+        public HelloHomeException(string code, string message, Exception innerException)
+            : base(message, innerException)
+        {
+            Code = code;
+        }
+    }
+}
---
+++ HelloHome.Central.Common/Exceptions/NodeNotFoundException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloHome.Common.Exceptions
+{
+    [Serializable]
+    public class NodeNotFoundException : HelloHomeException
+    {
+        public NodeNotFoundException(int rfAddress)
+            : base("NODE_NOT_FOUND", $"Node with rfId {rfAddress} not found.")
+        {
+
+        }
+        public NodeNotFoundException(long signature)
+            : base("NODE_NOT_FOUND", $"Node with signature {signature} not found.")
+        {
+
+        }
+    }
+}
