a10bf5df9df070441082cd419f32cb1aaac8ecce
Valery De Smedt <valery@indri.be>
2018-09-26 09:28:31 +0200

    Added logging to know when port is open

--- HelloHome.Central.Hub/MessageChannel/SerialPortMessageChannel/SerialByteStream.cs
+++ HelloHome.Central.Hub/MessageChannel/SerialPortMessageChannel/SerialByteStream.cs
@@ -2,21 +2,25 @@
 using System.Threading;
 using System.Threading.Tasks;
 using HelloHome.Central.Common.Configuration;
+using NLog;
 using RJCP.IO.Ports;
 
 namespace HelloHome.Central.Hub.MessageChannel.SerialPortMessageChannel
 {
     public class SerialByteStream : IByteStream, IDisposable
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
         private readonly SerialPortStream _port;
 
         public SerialByteStream(SerialConfig config)
         {
+            Logger.Info(() => $"Opening serial port {config.Port} with baudRate {config.BaudRate} (8N1)");
             _port = new SerialPortStream(config.Port, config.BaudRate, 8, Parity.None, StopBits.One)
             {
                 ReadTimeout = config.TimeOut
             };
-            _port.Open();            
+            _port.Open();
+            Logger.Info(() => $"Serial port {config.Port} opened");
         }
 
         public int Read(byte[] buffer, int offset, int cout)
