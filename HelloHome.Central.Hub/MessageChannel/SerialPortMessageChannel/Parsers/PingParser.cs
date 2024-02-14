a2996c756366fd2bdc51e7913fa97ab30d1bee8d
Valery De Smedt <valery@indri.be>
2024-02-14 08:15:51 +0100

    Ping command is 12 bytes instead of 11

--- HelloHome.Central.Hub/MessageChannel/SerialPortMessageChannel/Parsers/PingParser.cs
+++ HelloHome.Central.Hub/MessageChannel/SerialPortMessageChannel/Parsers/PingParser.cs
@@ -12,9 +12,10 @@ namespace HelloHome.Central.Hub.MessageChannel.SerialPortMessageChannel.Parsers
     [ParserFor(Constants.Message.Report.PingReport)]
     public class PingParser : IMessageParser
     {
+        //{from:2}{rssi:2}{msgType:1}{msgId:1}{millis:4}{CRLF:2}
         public IncomingMessage Parse(byte[] record)
         {
-            var expectedLenght = 11;
+            var expectedLenght = 12;
             if(record.Length != expectedLenght)
                 throw new ArgumentException($"{nameof(PingReport)} should be {expectedLenght} bytes long (was {record.Length})");
             return new PingReport() {
